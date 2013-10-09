17f45b489a73a6a2d360ac87e83e8648b401c70c
K. Arthur Endsley <kaendsle@mtu.edu>
2013-10-09 16:19:53 -0400

    Updated U.S. States geometries

--- scripts/flux_by_state/notes.bash
+++ scripts/flux_by_state/notes.bash
@@ -1,16 +1,19 @@
-# Get the Natural Earth dataset
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
-unzip ne_10m_admin_1_states_provinces.zip
+# Get the Natural Earth dataset (1:50m cultural without large lakes)
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_1_states_provinces_lakes.zip
+unzip ne_50m_admin_1_states_provinces_lakes.zip
 
 # Extract just American states
-ogr2ogr -f "ESRI Shapefile" -where "adm0_a3 IN ('USA')" us_states.shp ne_10m_admin_1_states_provinces.shp
+ogr2ogr -f "ESRI Shapefile" -where "sr_adm0_a3 IN ('USA')" us_states.shp ne_50m_admin_1_states_provinces_lakes.shp
+
+# Remove the old stuff
+rm ne_50m_admin_1_states_provinces_lakes.*
 
 # Convert to GeoJSON
 ogr2ogr -f "GeoJSON" us_states.json us_states.shp
 
-# Simplify to 10^-5 (0.00001) steradians, which is acceptable for this display
-# See: http://en.wikipedia.org/wiki/Steradian
-topojson --id-property su_a3 -s 0.00001 -p name=NAME -o us_states.topo.json us_states.json
+# Simplify to 10^-6 (0.000001) steradians, which is acceptable for this display
+# See: http://en.wikipedia.org/wiki/Steradian#SI_multiples
+topojson --id-property su_a3 -s 0.000001 -p name=NAME -o us_states.topo.json us_states.json
 
 # Make a GeoJSON copy with reduced precision
 geojson --precision 3 us_states.topo.json
