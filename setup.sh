b653493157a959ae6f47632f90a6fb4ef8130998
K. Arthur Endsley <kaendsle@mtu.edu>
2014-04-08 16:47:30 -0400

    Updated setup process to install GEOS, a dependency for Shapely

--- setup.sh
+++ setup.sh
@@ -8,6 +8,12 @@ sudo apt-get install gfortran libopenblas-dev liblapack-dev # Required for scipy
 sudo apt-get install libhdf5-dev
 sudo apt-get install libxml2 libxml2-dev libxslt1 libxslt1-dev
 
+# For Ubuntu 12.04 (precise) only
+echo "Installing geospatial system dependencies (GEOS)..."
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install libgeos-3.3.8 libgeos-c1
+
 echo "Installing MongoDB..."
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 # Create a /etc/apt/sources.list.d/10gen.list file and include the following line for the 10gen repository.
