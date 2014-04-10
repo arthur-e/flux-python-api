5cac55a52d6c0db4f6bc14d5061da532756b6815
K. Arthur Endsley <kaendsle@mtu.edu>
2014-04-10 15:57:02 -0400

    Updated workflow.py for Mike

--- workflow.py
+++ workflow.py
@@ -4,11 +4,11 @@ from fluxpy.mediators import *
 if __name__ == '__main__':
 
 #    # To import the 2004 CASA GFED run...
-#    path = '/gis_lab/project/NASA_ACOS_Visualization/Data/from_Vineet/data_casa_gfed_3hrly.mat'
-#    inst = SpatioTemporalMatrix(path, timestamp='2003-12-22T03:00:00',
-#        var_name='casa_gfed_2004')
-#    mediator = Grid4DMediator().save('casa_gfed_2004', inst)
+    path = '/net/nas3/data/gis_lab/project/NASA_ACOS_Visualization/Data/from_Vineet/data_casa_gfed_3hrly.mat'
+    inst = SpatioTemporalMatrix(path, timestamp='2003-12-22T03:00:00',
+        var_name='casa_gfed_2004')
+    mediator = Grid4DMediator().save('casa_gfed_2004', inst)
 
     # To import the sample uncertainty scenarios...
-    path = '/ws4/idata/fluxvis/casa_gfed_inversion_results/1.zerofull_casa_1pm_10twr/Month_Uncert1.mat'
-    inst = CovarianceMatrix(path, timestamp='2008-01', span='1M')
+#    path = '/ws4/idata/fluxvis/casa_gfed_inversion_results/1.zerofull_casa_1pm_10twr/Month_Uncert1.mat'
+#    inst = CovarianceMatrix(path, timestamp='2008-01', span='1M')
